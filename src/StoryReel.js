88eb8c9977e2ccf04701b495f9a8ede24d8ee3e5
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-10-03 20:47:27 -0500

    added widgets.js and modified storyreel.js

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -6,15 +6,15 @@ function StoryReel() {
     return (
         <div className="storyReel">
             {/* Story */}
-            <Story image="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-15/e35/117227248_586850415533303_3473043354800851414_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=qiw52tV7Xl4AX8dXhhY&_nc_tp=18&oh=fea18169003547d26a698d33200e1ed0&oe=5F84474B" profileSrc="https://avatars.githubusercontent.com/saviomartin" title="Savio Martin" />
+            <Story image="https://kaikucaffelatte.com/blog/wp-content/uploads/2020/03/shutterstock_510679489-scaled.jpg" profileSrc="https://avatars.githubusercontent.com/gilberthuarcaya" title="Gilbert Huarcaya" />
 
-            <Story image="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-15/e35/121000593_110923880688458_1897834268424628982_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=6ZKYQ8RaVcQAX9FUJVm&_nc_tp=18&oh=18d55d74d001e5eceaeb1d1fbad6a2ab&oe=5F83FCCA" profileSrc="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/118091999_304127827545656_7867854293078204270_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_ohc=Ew--seFc_pcAX_8WLGB&oh=b30ce23d49578d85ce5ff728a195f6a6&oe=5FAA29C3" title="Realme India" />
+            <Story image="http://univia.info/wp-content/uploads/2021/04/paisajes-expresivos-734x489-1.jpg" profileSrc="https://mott.pe/noticias/wp-content/uploads/2018/10/las-7-mejores-cuentas-de-fotografos-de-retrato-en-instagram-1.png" title="Realme" />
 
-            <Story image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png" profileSrc="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4" title="Sonny Sangha" />
+            <Story image="https://www.dzoom.org.es/wp-content/uploads/2017/07/seebensee-2384369-810x540.jpg" profileSrc="https://i.pinimg.com/236x/3f/2f/44/3f2f44f05a487a9d65f735aa87ac3b02.jpg" title="Sonny" />
 
-            <Story image="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.12442-15/e35/p1080x1080/95626377_698632864224608_4690720343234713657_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=eHSyhi2GhCUAX9zkkLs&_nc_tp=19&oh=4ffa6d4fb3d51e5f6049540e615b17b4&oe=5F83DF48" profileSrc="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-19/10729318_654650964633655_619168277_a.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_ohc=H2rUtmI3UIwAX8GvREE&oh=b9cd55ce58cdb2876c745013a8efced5&oe=5FACEE75" title="Microsoft" />
+            <Story image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwRfdhL0WdhjfpBvC8uGkfhoTOuOqB31n67g&usqp=CAU" profileSrc="https://img.freepik.com/foto-gratis/retrato-hombre-blanco-aislado_53876-40306.jpg?size=626&ext=jpg" title="Microsoft" />
 
-            <Story image="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/121060575_340014553945432_8028668953703766441_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=3WIclsEgKLcAX8ciTmK&_nc_tp=19&oh=b61f9daad4439b93983f2b06cb26b515&oe=5F84610B" profileSrc="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/102429948_1095179260864915_7316874323564026733_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_ohc=sEioj22PzHoAX_8PXTJ&oh=21a681263759d02d0adeb7568e8c2f35&oe=5FAC8F25" title="Sajon" />
+            <Story image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYNI7NtlCCbKAjuqgPPn0RJ9sW5wmsAR7DNw&usqp=CAU" profileSrc="https://d500.epimg.net/cincodias/imagenes/2019/05/28/lifestyle/1559073183_258744_1559073334_rrss_normal.jpg" title="Sayu" />
         </div>
     )
 }
--- src/Widgets.js
+++ src/Widgets.js
@@ -4,8 +4,8 @@ import './Widgets.css'
 function Widgets() {
     return (
         <div className="widgets">
-            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCleverProgrammerr%2F&amp;tabs=timeline&amp;width=340&amp;height=1500&amp;small_header=false&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId"
-                title="Clever Programmer Fb Page"
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com/MicrosoftLatam/?brand_redir=20528438720&amp;tabs=timeline&amp;width=340&amp;height=1500&amp;small_header=false&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId"
+                title="Microsoft Fb Page"
                 width="340"
                 height="100%"
                 style={{ border: 'none', overflow: 'hidden' }}
